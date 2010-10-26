81628c9645fda99cf2f0b6e7c37fb027fdd0fbe7
mrb <michaelrbernstein@gmail.com>
2010-10-26 15:45:10 -0400

    rb

---
+++ achievements.rb
@@ -0,0 +1,54 @@
+# An Abstract Achievements Engine
+# Include the gem in a Rails project
+# Bind hooks in the User model, is_achiever
+# Trigger hooks in other model/controller/view methods
+
+require 'rubygems'
+require 'redis'
+
+class Achievements
+  attr_accessor :redis
+  attr_accessor :achievements
+
+  def initialize
+    @redis ||= Redis.connect
+    @achievements = []
+  end
+
+  def self.redis
+    @redis
+  end
+
+  def bind(achievement_json)
+    @achievements
+  end
+  
+  def trigger
+    
+  end
+end
+
+class Achievement
+  attr_accessor :key
+  attr_accessor :threshold
+  
+  def initialize
+    
+  end
+end
+
+class Counter
+  attr_accessor :context
+  attr_accessor :user_id
+  attr_accessor :key
+
+  def initialize(context, user_id, key_prefix)
+    @key = "#{context}:user:#{user_id}:#{key_prefix}"
+    @redis = Achievements.redis
+  end
+  
+  def incr
+    @redis.incr @key
+  end
+end
+
