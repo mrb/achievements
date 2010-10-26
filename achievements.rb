cf5d6b7cafc8058d62155009c18f1b24922086d0
mrb <michaelrbernstein@gmail.com>
2010-10-26 16:12:53 -0400

    moduleized

--- achievements.rb
+++ achievements.rb
@@ -1,49 +1,59 @@
-# An Abstract Achievements Engine
-# Include the gem in a Rails project
-# Bind hooks in the User model, is_achiever
-# Trigger hooks in other model/controller/view methods
-
+# An Abstract, Redis-Backed Achievements Engine
 require 'rubygems'
 require 'redis'
 
-class Achievements
-  attr_accessor :redis
-  attr_accessor :achievements
-
-  def initialize
-    @redis ||= Redis.connect
-    @achievements = []
-  end
-
-  def bind(achievement_json)
-    @achievements
+module AchievementEngine
+  # Give the class that includes the AE some nice convenience class
+  # methods for instantiating an engine and adding achievements
+  def self.included(base)
+    base.extend IncludeClassMethods
   end
   
-  def trigger
+  # Convenience Class Methods for ActiveRecord::Base like User Classes
+  class IncludeClassMethods
     
   end
-end
-
-class Achievement
-  attr_accessor :key
-  attr_accessor :threshold
   
-  def initialize
+  class Achievements
+    attr_accessor :redis
+    attr_accessor :achievements
     
+    def initialize
+      @redis ||= Redis.connect
+      @achievements = []
+    end
+
+    def bind(achievement_hash)
+      if achievement = Achievement.new(achievement_hash)
+        @achievements << achievement
+      end
+    end
+
+    def trigger
+    
+    end
+  end
+
+  class Achievement
+    attr_accessor :key
+    attr_accessor :threshold
+  
+    def initialize
+    
+    end
+  end
+
+  class Counter
+    attr_accessor :context
+    attr_accessor :user_id
+    attr_accessor :key
+
+    def initialize(context, user_id, key_prefix)
+      @key = "#{context}:user:#{user_id}:#{key_prefix}"
+    end
+  
+    def incr
+  
+    end
   end
 end
-
-class Counter
-  attr_accessor :context
-  attr_accessor :user_id
-  attr_accessor :key
-
-  def initialize(context, user_id, key_prefix)
-    @key = "#{context}:user:#{user_id}:#{key_prefix}"
-  end
-  
-  def incr
-  
-  end
-end
-
