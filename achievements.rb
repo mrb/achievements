55018f3d7437c8e7c71d03d20d7296ea9385ccec
mrb <michaelrbernstein@gmail.com>
2010-10-27 13:23:01 -0400

    more structure

--- achievements.rb
+++ achievements.rb
@@ -3,22 +3,82 @@ require 'rubygems'
 require 'redis'
 
 module AchievementEngine
-  # Give the class that includes the AE some nice convenience class
-  # methods for instantiating an engine and adding achievements
-  def self.included(base)
-    base.extend IncludeClassMethods
-  end
+  module UserIncludes
+    # Convenience methods for instantiating an engine and adding achievements
+    def self.included(base)
+      base.extend IncludeClassMethods
+    end
   
-  # Convenience Class Methods for ActiveRecord::Base like User Classes
-  module IncludeClassMethods
-    def achievable
-      @achievements_engine = Achievements.new
+    # Convenience Class Methods for ActiveRecord::Base like User Classes
+    module IncludeClassMethods
+      # Instantiates the AchievementEngine and sets
+      # the contexts, which instantiate context specific counters.  Use
+      # only once.
+      # 
+      # achieveable {:contexts => [:context1,:context2]}
+      #
+      def achievable
+        @achievements_engine = Achievements.new
+      end
+
+      # Binds an achievement with a specific counter threshold. Use as
+      # many as you'd like.
+      #
+      # bind :context, :name, threshold
+      #
+      def bind(context, name, threshold)
+        @achievements_engine.bind(achievement_hash)
+      end
+
+      # Alternately, bind an entire array of achievement objects.  To
+      # use this, achievements must respond to the context, name, and
+      # threshold methods.
+      #
+      # For example, when using with rails:
+      #
+      # bind_all Achievement.all
+      #
+      def bind_all(object_array)
+        object_array.each do |object|
+          bind object.context, object.name, object.threshold
+        end
+      end
+
+      # Trigger a bound achievement method.  Since this is a class
+      # level method, you must include the user id along with the
+      # method call
+      #
+      # trigger user_id, context, name
+      #
+      def trigger(user_id, context, name)
+        
+      end
     end
 
-    def bind(achievement_hash)
-      @achievements_engine.bind(achievement_hash)
+    # User instance methods
+
+    # User instance level achievement trigger.  Automatically sends
+    # user id along with context and name to the AchievementEngine
+    def trigger(context,name)
+      
     end
   end
+  
+  module AchievementIncludes
+    # Convenience methods
+    def self.included(base)
+      base.extend IncludeClassMethods
+    end
+
+    module IncludeClassMethods
+      # Update the threshold for the AchievementEngine achievement
+      # that matches context, achievement, and sets threshold to new_threshold
+      def update_threshold(context, achievement, new_threshold)
+        # find achievement and update threshold
+      end 
+    end
+  end
+  
 
   # Achivements Interface Class
   class Achievements
